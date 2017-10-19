bbaeef06ee85ac13d0a924fcc571092278b4b106
Philip Sayre <phsayre@gmail.com>
2017-10-19 11:45:18 -0400

    Write Print method in PHSListNode class

--- PHSProj2/src/package2/PHSList2.java
+++ PHSProj2/src/package2/PHSList2.java
@@ -8,122 +8,63 @@ class PHSList2 {
 	
 	public PHSList2(Object obj)
 	  {
-		head = new PHSListNode(obj);
-		numNodes
+		head = new PHSListNode(obj, null);
+		numNodes = 1;
 	  }
 	
 	
-	public void addToEnd(Object obj) {
+	public void addToEnd(Object value) {
 		
 		PHSListNode temp = head;
-		head = new PHSListNode(obj);
+		head = new PHSListNode(value, null);
 		head.next = temp;
 		numNodes++;
 	}
 	
-	
-	public void allocateNewArray() {
-		items = Arrays.copyOf(items,  items.length * 2);
-	}
-	
-	
-	public void Print() {
-		if (items.length == 0) {
-			System.out.println("[]");
-		}
-		else {
-			String result = "[ ";
-			
-			for (int i = 0; i < numItems; i++) {
-				result += items[i].toString();
-				result += " ";
-			}
-			
-			result += "]";
-			
-			System.out.println(result);
-		}
-		
-	}
-	
-	
-    public void firstElement() {
-    	currentObject = 0;
-	}
-    
-    
-    public Object nextElement(int pointer) {
-		if (pointer < numItems) {
-			return items[pointer];
-		} 
-		else {
-			throw new ArrayIndexOutOfBoundsException();
-		}
-	}
-    
-    public boolean hasMoreElements(int currentObject, Object obj) {
-    	try {
-    		currentObject ++;
-    		items[currentObject] = obj;
-    		return true;
-    	}
-    	catch (Exception e){
-    		return false;
-    	}
-    }
-	
     
 	public static void main(String[] args) {
-		PHSList2 obj = new PHSList2();
+		PHSListNode <String> myList = new PHSListNode<String>("Honda",
+									  new PHSListNode<String>("Toyota",
+									  new PHSListNode<String>("Nissan",
+									  new PHSListNode<String>("BMW",
+									  new PHSListNode<String>("Daihatsu",
+									  new PHSListNode<String>("Subaru",
+									  new PHSListNode<String>("Chevrolet",
+									  new PHSListNode<String>("AMC", null))))))));
 		
-		//Test 1
-		obj.addToEnd(new String("Suzy"));
-		obj.addToEnd(new String("James"));
-		obj.addToEnd(new String("Reginald"));
-		
-		obj.Print();
-		
-		//Test2
-		obj = null;
-		obj = new PHSList2();
-	
-		obj.Print();
-		
-		//Test3
-		obj = null;
-		obj = new PHSList2();
-		
-		obj.addToEnd(new String("Suzy"));
-		obj.addToEnd(new String("James"));
-		obj.addToEnd(new String("Reginald"));
-		obj.addToEnd(new String("Anne"));
-		obj.addToEnd(new String("Joe"));
-		obj.addToEnd(new String("Ryan"));
-		obj.addToEnd(new String("Matt"));
-		obj.addToEnd(new String("Donald"));
-		obj.addToEnd(new String("Jennifer"));
-		obj.addToEnd(new String("Winston"));
-		obj.addToEnd(new String("Camel"));
-		obj.addToEnd(new String("Jessica"));
-		obj.addToEnd(new String("Sharlene"));
-		obj.addToEnd(new String("Victoria"));
-		
-		obj.Print();
+		myList.Print(myList);
 		
 	}
 }
 
-class PHSListNode {
 
-	PHSListNode next;
+class PHSListNode <E> {
+
+	PHSListNode<E> next;
 	private Object value;
 	
-	public PHSListNode(Object obj, PHSListNode newNext) {
-		value = obj;
-		next = newNext;
+	public PHSListNode(Object newVal, PHSListNode<E> nextVal) {
+		value = newVal;
+		next = nextVal;
 	}
 	
+	public void Print(PHSListNode<String> myNode) {
+		String result = "[ ";
+		
+		while (myNode != null) {
+			result += myNode.getValue().toString();
+			result += " ";
+			myNode = myNode.getNext();
+		}
+		
+		result += "]";
+		System.out.println(result);
+		
+	}
+
 	public Object getValue() {return value;}
-	public PHSListNode getNext() {return next;}
+	public PHSListNode<E> getNext() {return next;}
+	public void setValue(Object newVal) {value = newVal;}
+	public void setNext(PHSListNode<E> newNext) {next = newNext;}
 	
 }
